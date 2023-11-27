dd5e21dcdbc9d9ee5381564d969c027833472427
sarpergulfirat <sarpergulfirat@gmail.com>
2023-11-27 22:18:48 +0300

    Pagination added, sorting functionality implemented, product detail page completed, add to cart button is working.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Navbar from "./components/navbar/Navbar"
 import PageContainer from "./containers/PageContainer";
+import Detail from "./pages/Detail"
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
 
@@ -13,6 +14,7 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/products/:id" element={<Detail />} />
         </Routes>
       </Router>
       </PageContainer>
--- src/components/home/Category.jsx
+++ src/components/home/Category.jsx
@@ -1,25 +1,25 @@
-import React from 'react'
-import { useEffect } from 'react';
+import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { getCategories } from '../../redux/categorySlice';
 
-const Category = () => {
+const Category = ({setCategory}) => {
+
   const dispatch = useDispatch();
-  const { categories } = useSelector(state => state.categories)
+  const {categories} = useSelector(state => state.categories)
+
+  console.log(categories, "categories")
 
   useEffect(() => {
     dispatch(getCategories())
   }, [dispatch])
 
-  // console.log(categories, "categories")
-
   return (
-    <div className='w-1/6 bg-gray-100 p-4 max-h-screen'>
-      <div className='border-b pb-1 text-xl font-bold'>CATEGORY</div>
+    <div className='w-1/6 bg-gray-100 p-4'>
+      <div className='border-b pb-1 px-2 text-xl font-bold'>Kategori</div>
       {
-        categories?.map((category, i) => (
-          <div className='text-xl cursor-pointer hover:bg-gray-200 p-2' key={i}>{category}</div>
-        ))
+      categories?.map((category,i) => (
+        <div onClick={() => setCategory(category)} className='text-lg cursor-pointer hover:bg-gray-200 p-2' key={i}>{category}</div>
+      ))
       }
     </div>
   )
